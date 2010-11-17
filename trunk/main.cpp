e293320800bf7ae44e0aeeba6eb94b5347df4035
tomas.cerevka@gmail.com <tomas.cerevka@gmail.com@92fc87e4-2479-d7bd-aa1e-631700c1f2c1>
2010-11-17 14:51:37 +0000

    Vytvoreno zakladni graficke rozhrani s menu, ktere je napojeno na sloty. Aplikace je pripravena na multijazycne prostredi - prilozeny aktulni slovniky a zkompilovane preklady.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,8 +1,33 @@
-#include <QtCore/QCoreApplication>
+#include <QtCore>
+#include <QtGui>
+#include <QTranslator>
 
+#include "view/mainwindow.h"
+
+/**
+  * Vstupni bod do systemu.
+  * @param argc Pocet argumentu prikazove radky.
+  * @param argv[] Pole parametru prikazove radky.
+  * @return Navratova hodnota programu.
+  */
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    // Inicializace Qt.
+    QApplication app(argc, argv);
 
-    return a.exec();
+    // Inicializuje se translator.
+    QTranslator translator;
+    translator.load("../felchess_en");
+    app.installTranslator(&translator);
+
+
+    // Vytvori se okno aplikace.
+    MainWindow * mainWindow = new MainWindow();
+    mainWindow->show();
+
+    // Spusteni Qt.
+    return app.exec();
+
+    // Uvolneni pameti alokovanych objektu.
+    delete mainWindow;
 }
--- trunk/view/mainwindow.h
+++ trunk/view/mainwindow.h
@@ -1,10 +1,75 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-class MainWindow
+#include <QMainWindow>
+#include <QtGui>
+
+/**
+  * Hlavni okno programu, ktere zajistuje konstru GUI.
+  */
+class MainWindow : public QMainWindow
 {
+    Q_OBJECT
+
 public:
+
+    /**
+      * Vytvoreni noveho okna.
+      */
     MainWindow();
+
+private:
+
+    /**
+      * Vytvoreni menu hlavniho okna.
+      */
+    void createMenuBar();
+
+    QMenu * gameMenu;
+    QMenu * toolMenu;
+    QMenu * helpMenu;
+
+    QAction * newGameAct;
+    QAction * exitAct;
+
+    QAction * switchLanguageAct;
+    QAction * showSettingsAct;
+
+    QAction * showAboutAct;
+    QAction * showHelpAct;
+
+private slots:
+
+    /**
+      * Slot, ktery zpracovava zalozeni nove hry.
+      */
+    void newGameSlot(void);
+
+    /**
+      * Slot, ktery zpracovava ukonceni programu.
+      */
+    void exitSlot(void);
+
+    /**
+      * Slot, ktery zpracovava prepnuti jazyka.
+      */
+    void switchLanguageSlot(void);
+
+    /**
+      * Slot, ktery zpracovava vstup do nastaveni.
+      */
+    void showSettingsSlot(void);
+
+    /**
+      * Slot, ktery zpracovava zobrazni informaci o aplikaci.
+      */
+    void showAboutSlot(void);
+
+    /**
+      * Slot, ktery zpracovava zobrazeni napovedy.
+      */
+    void showHelpSlot(void);
+
 };
 
 #endif // MAINWINDOW_H
