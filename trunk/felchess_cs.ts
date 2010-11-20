0c9b808f6603b81be0397cbd7b6b7f6ff4cab025
tomas.cerevka@gmail.com <tomas.cerevka@gmail.com@92fc87e4-2479-d7bd-aa1e-631700c1f2c1>
2010-11-20 21:14:32 +0000

    Jmeno jazyka se vybira ze slovniku. Aktualizovany preklady.

--- trunk/felchess_cs.ts
+++ trunk/felchess_cs.ts
@@ -4,99 +4,79 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <source>window_title</source>
-        <translation type="obsolete">Šachy</translation>
-    </message>
-    <message>
-        <location filename="view/mainwindow.cpp" line="8"/>
+        <location filename="view/mainwindow.cpp" line="111"/>
         <source>windowTitle</source>
         <translation>Šachy</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="21"/>
+        <location filename="view/mainwindow.cpp" line="117"/>
         <source>newGame</source>
         <translation>Nová hra</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="22"/>
+        <location filename="view/mainwindow.cpp" line="118"/>
         <source>CTRL+N</source>
         <translation>CTRL+N</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="23"/>
+        <location filename="view/mainwindow.cpp" line="119"/>
         <source>newGameStatusTip</source>
         <translation>Založit novou hru</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="27"/>
+        <location filename="view/mainwindow.cpp" line="122"/>
         <source>CTRL+Q</source>
         <translation>CTRL+Q</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="28"/>
+        <location filename="view/mainwindow.cpp" line="123"/>
         <source>exitStatusTip</source>
         <translation>Ukončit aplikaci</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="79"/>
+        <location filename="view/mainwindow.cpp" line="112"/>
         <source>gameMenu</source>
         <translation>Hra</translation>
     </message>
     <message>
         <location filename="view/mainwindow.cpp" line="121"/>
-        <source>switchLanguageMessageBox</source>
-        <translation>Změna jazyka se projeví po restartu aplikace.</translation>
-    </message>
-    <message>
-        <source>newGameAct</source>
-        <translation type="obsolete">Nová hra</translation>
-    </message>
-    <message>
-        <location filename="view/mainwindow.cpp" line="26"/>
         <source>exitAct</source>
         <translation>Konec</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="85"/>
+        <location filename="view/mainwindow.cpp" line="113"/>
         <source>toolMenu</source>
         <translation>Nástroje</translation>
     </message>
     <message>
-        <source>languageAct</source>
-        <translation type="obsolete">Jazyk</translation>
-    </message>
-    <message>
-        <location filename="view/mainwindow.cpp" line="66"/>
+        <location filename="view/mainwindow.cpp" line="125"/>
         <source>settingAct</source>
         <translation>Nastavení</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="87"/>
+        <location filename="view/mainwindow.cpp" line="114"/>
         <source>languageMenu</source>
         <translation>Jazyky</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="100"/>
+        <location filename="view/mainwindow.cpp" line="54"/>
+        <source>dictionaryName</source>
+        <translation>Česky</translation>
+    </message>
+    <message>
+        <location filename="view/mainwindow.cpp" line="115"/>
         <source>helpMenu</source>
         <translation>Nápověda</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="70"/>
+        <location filename="view/mainwindow.cpp" line="127"/>
         <source>aboutAct</source>
         <translation>O aplikaci</translation>
     </message>
     <message>
-        <location filename="view/mainwindow.cpp" line="72"/>
+        <location filename="view/mainwindow.cpp" line="128"/>
         <source>helpAct</source>
         <translation>Nápověda</translation>
-    </message>
-    <message>
-        <source>felchess_cs.qm</source>
-        <translation>Česky</translation>
-    </message>
-    <message>
-        <source>felchess_en.qm</source>
-        <translation>English</translation>
     </message>
 </context>
 </TS>
