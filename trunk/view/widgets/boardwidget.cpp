1ad0a05423166d18b04385dd084fe7a1d99a3d8b
tomas.cerevka@gmail.com <tomas.cerevka@gmail.com@92fc87e4-2479-d7bd-aa1e-631700c1f2c1>
2010-12-08 14:22:50 +0000

    Odstraněno celé OpenGL.

--- trunk/view/widgets/boardwidget.cpp
+++ trunk/view/widgets/boardwidget.cpp
@@ -1,11 +1,8 @@
 #include "boardwidget.h"
 
-BoardWidget::BoardWidget(QWidget *parent): QGLWidget(parent)
+BoardWidget::BoardWidget(QWidget *parent): QWidget(parent)
 {
-    // Aktivace double bufferingu a hloukoveho bufferu.
-    setFormat(QGLFormat(QGL::DoubleBuffer | QGL::DepthBuffer));
-
-    // Nastaveni velikosti widgetu.
+        // Nastaveni velikosti widgetu.
     setMinimumSize(400, 400);
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
@@ -15,79 +12,19 @@ BoardWidget::BoardWidget(QWidget *parent): QGLWidget(parent)
 
 BoardWidget::~BoardWidget(void)
 {
-    // Znici se kreslitko vytvorene pri inicializaci OpenGL.
-    delete glPainter;
 
 }
 
-void BoardWidget::initializeGL(void)
+void BoardWidget::resize(int width, int height)
 {
-    // Zapnuti hloubkoveho bufferu.
-    glEnable(GL_DEPTH_TEST);
+    int side = qMin(width, height);   
 
-    // Nastaveni barvy pozadi.
-    qglClearColor(QColor::fromRgb(242, 241, 240));
-
-    // Vymazani bufferu.
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Vytvori se kreslitko.
-    glPainter = new Painter(this, Painter::MODE_2D);
+    //resize(side, side);
 }
 
-void BoardWidget::resizeGL(int width, int height)
+void BoardWidget::paint(void)
 {
-    int side = qMin(width, height);
 
-    // Upravi se velikost vykreslovane oblasti.
-    glViewport((width - side) / 2, (height - side) / 2, side, side);
-
-    // Matice pro pozici pozorovatele...
-    glMatrixMode(GL_PROJECTION);
-    // ... se vynuluje na jednotkovou.
-    glLoadIdentity();
-    // Prepne se zpet na matici pro transformaci objektu.
-    glMatrixMode(GL_MODELVIEW);
-
-    resize(side, side);
-}
-
-void BoardWidget::paintGL(void)
-{
-    QPainter painter(this);
-
-    // Vycisteni bufferu.
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Matice pro transformace objektu...
-    glMatrixMode(GL_MODELVIEW);
-    // ... se vynuluje na jednotkovou.
-    glLoadIdentity();
-
-    glPainter->drawChessboard();
-
-    QSvgRenderer * renderer = new QSvgRenderer(QString(":/pieces/white/archer"));
-    painter.setRenderHint(QPainter::Antialiasing);
-
-    QPixmap pixmap(width()/8, height()/8);
-    pixmap.fill(Qt::transparent);
-    QPainter pixPainter(&pixmap);
-    renderer->render(&pixPainter);
-
-
-    painter.save();
-
-    //painter.drawTiledPixmap(50, 0, pixmap.width(), pixmap.height(), pixmap);;
-
-    //renderer->render(&painter);
-
-
-   QPoint point(3, 3);
-   APiece * piece = new King(false, point, this);
-   glPainter->drawPiece(&painter, piece);
-
-   painter.restore();
-  painter.end();
 
 }
 
